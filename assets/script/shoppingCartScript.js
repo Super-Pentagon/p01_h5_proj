5752c6c7ab4895a6874700eef8fd1c8d24aef5b8
MC-LingFeng <1294432739@qq.com>
2020-06-28 13:05:38 +0800

    价格数量修改

--- assets/script/shoppingCartScript.js
+++ assets/script/shoppingCartScript.js
@@ -1,38 +1,132 @@
 window.onload = function(){
-    // var shopSele = document.querySelector(".shopSele");
-    var shopSele = document.querySelectorAll(".shopSele");
+
+ 
     
-    var arr = new Array(shopSele.length);
-    for(var i=0;i< shopSele.length;i++){
-        arr[i] =  $(shopSele[i]).prop("checked");
-        bind(shopSele[i],"click",function(){
-           
-            if( $(shopSele[i]).prop("checked")){
-                
-                $(shopSele[i]).prop("checked") = false;
-                console.log(0);
-                
-            
+    amount();
+    check();
+    checkAll()
+    add(15);
+    sub(15);
+    del() ;
+    function checkAll() {
+        var checkedAllBox = document.getElementById("checkedAllBox");
+        var shopSele = document.querySelectorAll(".shopSele");
+        checkedAllBox.onclick = function(){
+            for(var i =0;i<shopSele.length;i++){
+                shopSele[i].checked = this.checked;
             }
-        });
-        
+            amount();
     }
-
-
-    function bind(obj, eventStr, callBack) {
-        if (obj.addEventListener){
-            //大部分浏览器
-            obj.addEventListener(eventStr, callBack, false);
-        }else{
-            /**
-             * this是谁由调用方式决定
-             */
-            //IE8以下
-            obj.attachEvent("on"+eventStr, function () {
-                    callBack().call(obj);
-            });
-
+    }
+    function check(){
+        var shopSele = document.querySelectorAll(".shopSele");
+        for(var i=0;i< shopSele.length;i++){
+            (function(i){
+                shopSele[i].onclick = function(){
+                    if(this.c== 0){
+                        this.c = 1;
+                        this.checked = false;
+                        // console.log(this.c);
+                        // console.log(this.checked);
+                        amount();
+                        
+                    }else{
+                        this.c = 0;
+                        this.checked = true;
+                        amount();
+                    }
+                    
+                }
+            })(i)    
         }
-
     }
+ 
+   function del() {
+    var del = document.getElementById("del");
+    var shopSele = document.querySelectorAll(".shopSele");
+       del.onclick = function(){
+        for(var i =0 ;i<shopSele.length;i++){
+            if(shopSele[i].checked == true){
+                shopSele[i].parentNode.parentNode.remove();
+                amount();
+            }
+        }
+    }
+  
+   }
+    
+    function amount() {
+        var mony = 0;
+        var shopSele = document.querySelectorAll(".shopSele");
+        var prices = document.querySelectorAll('.shopPrice');
+        var amounts = document.querySelector('.amount');
+        var checkedAllBox = document.getElementById("checkedAllBox");
+        for(var i =0 ; i<shopSele.length;i++){
+            if(shopSele[i].checked){
+                mony += parseInt(prices[i].innerText); 
+            }else {
+              
+            }
+        }
+       
+        amounts.innerHTML = "￥"+mony;
+    }
+
+    function add(shopOnePrice){
+        var add = document.querySelectorAll('.add');
+        var nums = document.querySelectorAll('.shopNum');
+        var prices = document.querySelectorAll('.shopPrice');
+        
+        for(var i = 0 ;i<add.length;i++){
+            (function(i){
+               
+                
+                add[i].onclick = function(){
+                    // console.log(i);
+                    var num=parseInt(nums[i].innerText);
+                    
+                    var price = parseInt(prices[i].innerText);
+                    
+                    nums[i].innerHTML = num+1;   
+                    prices[i].innerHTML = price +shopOnePrice;
+                    amount();
+   
+                    
+                }
+            })(i)
+          
+        }
+    }
+    function sub(shopOnePrice){
+        var sub = document.querySelectorAll(".minus");
+        var nums = document.querySelectorAll('.shopNum');
+        var prices = document.querySelectorAll('.shopPrice');
+        for(var i = 0 ;i<sub.length;i++){
+            (function(i){
+               
+                
+                sub[i].onclick = function(){
+                   
+                    
+                    var num=parseInt(nums[i].innerText);
+                    var price = parseInt(prices[i].innerText);
+                    if(num == 0){
+                        shopSele[i].parentNode.parentNode.remove();
+                    }else{
+                        nums[i].innerHTML = num-1;   
+                        prices[i].innerHTML = price -shopOnePrice;
+                       amount();
+                    }
+                
+                 
+                    // console.log(price);
+                    
+                    
+                }
+            })(i)
+          
+        }
+    }
+    
+    
 }
